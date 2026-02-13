782b26d385d2f1ea68db5f70583f922f084b5f8b
InvYouDrkTea <linzishenlm@outlook.com>
2026-02-13 18:38:05 +0800

    By the time the repository was created, the project was basically completed

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, render_template, send_file
+
+app = Flask(__name__)
+
+@app.route("/")
+def default():
+    return redirect("/html/WYshd.html")
+
+@app.route("/html/<filename>")
+def html(filename):
+    return render_template(filename)
+
+@app.route("/images/<filename>")
+def images(filename):
+    return send_file("images/"+filename)
+
+if __name__ == "__main__":
+    app.run()
